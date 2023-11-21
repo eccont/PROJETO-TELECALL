5087cb647ff0b0767daae5c90f89bbc35634f6e8
Erick Contrera <erickc.contrera@gmail.com>
2023-11-21 00:29:36 -0300

    Update userLogado.js

--- JS/userLogado.js
+++ JS/userLogado.js
@@ -3,10 +3,7 @@ const campoLogado = document.getElementById("logged");
 const userLogado = JSON.parse(sessionStorage.getItem("logado"));
 const dados = JSON.parse(localStorage.getItem("dadosForm"));
 
-console.log(userLogado);
-
 if (userLogado === true) {
   campoLogado.innerHTML = "Boas vindas, " + dados["nome"];
   listDropdown.innerHTML = "Sair";
-  
 }
