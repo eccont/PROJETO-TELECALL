a8cfa64c4c9f2eaec04801a5adb24b7e2e6b84d8
Erick Contrera <101820369+eccont@users.noreply.github.com>
2023-11-22 10:44:31 -0300

    Update visualizar-senha.js

--- JS/visualizar-senha.js
+++ JS/visualizar-senha.js
@@ -5,21 +5,25 @@ const eye2 = document.getElementById("eye2")
 function showHide1() {
   if (password[0].type === "password") {
     password[0].setAttribute("type", "text")
-    eye1.classList.add("hide")
+    eye1.classList.remove("fa-eye")
+    eye1.classList.add("fa-eye-slash")
+	
+
   } else {
     password[0].setAttribute("type", "password")
-    eye1.classList.remove("hide")
+    eye1.classList.remove("fa-eye-slash")
+    eye1.classList.add("fa-eye")
   }
 }
 
 function showHide2() {
   if (password[1].type === "password") {
     password[1].setAttribute("type", "text")
-    eye2.classList.add("hide")
+    eye2.classList.remove("fa-eye")
+    eye2.classList.add("fa-eye-slash")
   } else {
     password[1].setAttribute("type", "password")
-    eye2.classList.remove("hide")
+    eye2.classList.remove("fa-eye-slash")
+    eye2.classList.add("fa-eye")
   }
 }
-
-
